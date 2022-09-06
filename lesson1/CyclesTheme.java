46e511ca5b1cf3243ec28df247542c487e36eeea
rbabenko <sinmakbra@gmail.com>
2022-09-06 10:24:54 +0300

    ver2

--- lesson1/CyclesTheme.java
+++ lesson1/CyclesTheme.java
@@ -16,7 +16,7 @@ public class CyclesTheme {
             }
             x++;
         } while (x <= 21);
-        System.out.println("Сумма четных чисел = " + sumEven + ", сумма нечетных чисел = " + sumOdd);
+        System.out.println("В промежутке [-10, 21] сумма четных чисел = " + sumEven + ", сумма нечетных чисел = " + sumOdd);
 
         System.out.println("\n2. Вывод чисел в интервале (min и max) в порядке убывания");
         int a = 10;
@@ -33,12 +33,13 @@ public class CyclesTheme {
         System.out.println("\n3. Вывод реверсивного числа и суммы его цифр");
         int number = 1234;
         int sum = 0;
+        System.out.print("Исходное число в обратном порядке: ");
         while (number >= 1) {
             sum += number % 10;
             System.out.print(number % 10);
             number /= 10;
         }
-        System.out.println("\n" + sum);
+        System.out.println("\nСумма цифр: " + sum);
         
         System.out.println("\n4. Вывод чисел на консоль в несколько строк");
         int j = 0;
@@ -55,5 +56,57 @@ public class CyclesTheme {
                 j++;
             }
         }
+
+        System.out.println("\n5. Проверка количества единиц на четность");
+        number = 3141591;
+        int source = number;
+        int countOne = 0;
+        while (number > 0) {
+            if (number % 10 == 1) {
+                countOne++;
+            }
+            number /= 10;
+        }
+        if (countOne % 2 == 0) {
+            System.out.printf("Число %d содержит %s количество единиц\n", source, "четное");
+        } else {
+            System.out.printf("Число %d содержит %s количество единиц\n", source, "нечетное");
+        }
+
+        System.out.println("\n6. Отображение фигур в консоли");
+        char symbol = '*';
+        System.out.println("прямоугольник:");
+        for (int i = 0; i < 5; i++) {
+            for (int k = 0; k < 10; k++) {
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+        System.out.println("прямоугольный треугольник:");
+        symbol = '#';
+        int row = 5;
+        while (row > 0) {
+            int column = row;
+            while (column > 0) {
+                System.out.print(symbol);
+                --column;
+            }
+            System.out.println();
+            --row;
+        }
+        System.out.println("треугольник:");
+        symbol = '$';
+        row = 1;
+        int column = 0;
+        j = 0;
+        do {
+            column = (row <= 3) ? 1 : column + (j++ - 1);
+            do {
+                System.out.print(symbol);
+                ++column;
+            } while (column <= row);
+            System.out.println();
+            ++row;
+        } while (row <= 5);
     }
  }
