20af94f62e33d3d8fdf26cd5132c2d01c0675640
sinmakbra <sinmakbra@gmail.com>
2022-10-19 00:04:59 +0300

    correction ver6

--- lesson1/MyFirstGame.java
+++ lesson1/MyFirstGame.java
@@ -1,9 +1,7 @@
 public class MyFirstGame {
     public static void main(String[] args) {
-        final int startRange = 0;
-        final int endRange = 100;
-        int copyStartRange = startRange;
-        int copyEndRange = endRange;
+        int startRange = 0;
+        int endRange = 100;
         int inputNumber = 58;
         int hiddenNumber = 1 + (int) (Math.random() * 100);
         System.out.println("Игра \"Угадай число\"\n");
@@ -11,12 +9,12 @@ public class MyFirstGame {
         while (inputNumber != hiddenNumber) {
             if (inputNumber < hiddenNumber) {
                 System.out.printf("Число %d меньше того, что загадал компьютер\n", inputNumber);
-                copyStartRange = inputNumber;
-                inputNumber = (copyEndRange - inputNumber) / 2 + inputNumber;
+                startRange = inputNumber;
+                inputNumber = (endRange - inputNumber) / 2 + inputNumber;
             } else if (inputNumber > hiddenNumber) {
                 System.out.printf("Число %d больше того, что загадал компьютер\n", inputNumber);
-                copyEndRange = inputNumber;
-                inputNumber = (inputNumber - copyStartRange) / 2;
+                endRange = inputNumber;
+                inputNumber = (inputNumber - startRange) / 2;
             }
         }
         System.out.printf("Вы победили!. Загаданное число: %d\nw", inputNumber);
