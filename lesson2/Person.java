9935bd50f9a252c82c1cb362a4616efe0754e702
sinmakbra <sinmakbra@gmail.com>
2022-10-20 22:11:04 +0300

    add Person

---
+++ lesson2/Person.java
@@ -0,0 +1,29 @@
+package lesson2;
+
+public class Person {
+    String sex = "male";
+    String name = "Jone";
+    double height = 1.82;
+    double weight = 76.5;
+    int age = 24;
+    
+    void move() {
+        System.out.println("move step");
+    }
+
+    void sit() {
+        System.out.println("sit down");
+    }
+
+    void run() {
+        System.out.println("run");
+    }
+
+    void speak() {
+        System.out.println("speak");
+    }
+
+    void learn() {
+        System.out.println("learn Java");
+    }
+}
