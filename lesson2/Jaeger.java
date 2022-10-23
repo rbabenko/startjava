af4da0ea8098cd7a769b984fa6100df6c827bdc7
sinmakbra <sinmakbra@gmail.com>
2022-10-23 22:51:35 +0300

    Jaeger

--- lesson2/Jaeger.java
+++ lesson2/Jaeger.java
@@ -1,5 +1,110 @@
 package lesson2;
 
 public class Jaeger {
+    private String name;
+    private String mark;
+    private double height;
+    private double weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public Jaeger() {}
+
+    public Jaeger(String name, String mark, double height, double weight, int speed, int strength, int armor) {
+        this.name = name;
+        this.mark = mark;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public void move() {
+        System.out.println("Идти");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void jump() {
+        System.out.println("Прыгать");
+    }
+
+    public void kill() {
+        System.out.println("Убивать");
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMark() {
+        return this.mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return this.strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getArmor() {
+        return this.armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
     
+
+    @Override
+    public String toString() {
+        return "{" +
+            " name='" + getName() + "'" +
+            ", mark='" + getMark() + "'" +
+            ", height='" + getHeight() + "'" +
+            ", weight='" + getWeight() + "'" +
+            ", speed='" + getSpeed() + "'" +
+            ", strength='" + getStrength() + "'" +
+            ", armor='" + getArmor() + "'" +
+            "}";
+    }
 }
--- lesson2/JaegerTest.java
+++ lesson2/JaegerTest.java
@@ -1,5 +1,28 @@
 package lesson2;
 
 public class JaegerTest {
-    
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setName("Gipsy Danger");
+        jaeger1.setMark("Mark-3");
+        jaeger1.setHeight(79.25);
+        jaeger1.setWeight(1.980);
+        jaeger1.setSpeed(7);
+        jaeger1.setStrength(8);
+        jaeger1.setArmor(6);
+
+        Jaeger jaeger2 = new Jaeger("Striker Eureka", "Mark-5", 76.2, 1.850, 10, 10, 9);
+
+        jaeger1.move();
+        jaeger1.run();
+
+        jaeger2.jump();
+        jaeger2.kill();
+
+        jaeger1.setArmor(10);
+        System.out.println(jaeger1.getMark());
+
+        jaeger2.setSpeed(2);
+        System.out.println(jaeger2.getName());
+    }
 }
