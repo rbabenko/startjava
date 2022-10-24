317925eb86748b12d4e6a7a46c62d895878a2b67
sinmakbra <sinmakbra@gmail.com>
2022-10-24 23:14:32 +0300

    correction ver1

--- lesson2/GuessNumberTest.java
+++ lesson2/GuessNumberTest.java
@@ -8,14 +8,14 @@ public class GuessNumberTest {
         String continuation;
 
         System.out.println("Игра \"Угадай число\"\n");
-
-        System.out.print("Введите имя 1-го игрока: ");
-        Player player1 = new Player(scanner.nextLine());
-
-        System.out.print("Введите имя 2-го игрока: ");
-        Player player2 = new Player(scanner.nextLine());
         
-        do {        
+        do {
+            System.out.print("Введите имя 1-го игрока: ");
+            Player player1 = new Player(scanner.nextLine());
+
+            System.out.print("Введите имя 2-го игрока: ");
+            Player player2 = new Player(scanner.nextLine());
+            
             GuessNumber game = new GuessNumber(player1, player2);
 
             game.execute();
