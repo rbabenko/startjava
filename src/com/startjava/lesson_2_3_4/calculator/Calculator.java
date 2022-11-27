f5ed37a16717f66369d8ded0018d9123af45201b
rbabenko <sinmakbra@gmail.com>
2022-11-27 11:36:04 +0300

    hw4

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,15 +1,38 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    public static int calculate(int a, int b, char sign) throws Exception {
-        int result = switch (sign) {
-            case '+' -> a + b;
-            case '-' -> a - b;
-            case '*' -> Math.multiplyExact(a, b);
-            case '/' -> Math.floorDiv(a, b);
-            case '^' -> (int) Math.pow(a, b);
-            default -> throw new Exception();
-        };
-        return result;
+    public static void calculate(String expression) {
+        String[] elements = expression.split("\s");
+        if (elements.length != 3) {
+            System.out.println("Введено выражение, которое не соответствует требуемому формату. Просьба " +
+                    "ввести корректное выражение");
+            return;
+        }
+
+        try {
+            int a = Integer.parseInt(elements[0]);
+            char sign = elements[1].charAt(0);
+            int b = Integer.parseInt(elements[2]);
+
+            if (!(a > 0 && b > 0)) {
+                throw new NumberFormatException();
+            }
+
+            int result = switch (sign) {
+                case '+' -> a + b;
+                case '-' -> a - b;
+                case '*' -> Math.multiplyExact(a, b);
+                case '/' -> Math.floorDiv(a, b);
+                case '^' -> (int) Math.pow(a, b);
+                default -> throw new IllegalStateException();
+            };
+
+            System.out.println(a + " " + sign + " " + b + " = " + result);
+        } catch (NumberFormatException e) {
+            System.out.println("Операнды должны быть целыми положительными числами. Просьба ввести" +
+                    " корректные значения");
+        } catch (IllegalStateException e) {
+            System.out.println("Введен неверный метематический оператор. Просьба ввести корректный оператор");
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -11,30 +11,7 @@ public class CalculatorTest {
         do {
             System.out.print("\nВведите математическое выражение: ");
             String expression = scanner.nextLine();
-            String[] elements = expression.split("\s");
-            if (elements.length != 3) {
-                System.out.println("Введено выражение, которое не соответствует требуемому формату. Просьба " +
-                        "ввести корректное выражение");
-                continue;
-            }
-            try {
-                int a = Integer.parseInt(elements[0]);
-                char sign = elements[1].charAt(0);
-                int b = Integer.parseInt(elements[2]);
-
-                if (!(a > 0 && b > 0)) {
-                    throw new NumberFormatException();
-                }
-
-                System.out.println(a + " " + sign + " " + b + " = " + Calculator.calculate(a, b, sign));
-            } catch (NumberFormatException e) {
-                System.out.println("Операнды должны быть целыми положительными числами. Просьба ввести" +
-                        " корректные значения");
-                continue;
-            } catch (Exception e) {
-                System.out.println("Введен неверный метематический оператор. Просьба ввести корректный оператор");
-                continue;
-            }
+            Calculator.calculate(expression);
 
             do {
                 System.out.print("Хотите продолжить вычисления? [yes/no]: ");
